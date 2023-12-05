ef00dbc99ef0c2500940ed55e29d05a283c94eb4
vnjfgh37 <64977330+vnjfgh37@users.noreply.github.com>
2023-12-06 00:38:58 +0900

    Make coffee service.

--- FirstProject/src/main/java/com/example/FirstProject/api/CoffeeApiController.java
+++ FirstProject/src/main/java/com/example/FirstProject/api/CoffeeApiController.java
@@ -3,6 +3,7 @@ package com.example.FirstProject.api;
 import com.example.FirstProject.dto.CoffeeForm;
 import com.example.FirstProject.entity.Coffee;
 import com.example.FirstProject.repository.CoffeeRepository;
+import com.example.FirstProject.service.CoffeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,19 +18,19 @@ import java.util.List;
 public class CoffeeApiController {
 
     @Autowired
-    private CoffeeRepository coffeeRepository;
+    private CoffeeService coffeeService;
 
     //GET
     @GetMapping("/coffees")
     public List<Coffee> index(){
-        return coffeeRepository.findAll();
+        return coffeeService.index();
     }
     @GetMapping("/coffees/{id}")
     public ResponseEntity<Coffee> show(@PathVariable Long id){
-        Coffee coffee = coffeeRepository.findById(id).orElse(null);
+        Coffee coffee = coffeeService.show(id);
         if(coffee == null){
             log.info("Bad Request Id No: {}", id);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
         return ResponseEntity.status(HttpStatus.OK).body(coffee);
     }
@@ -37,39 +38,22 @@ public class CoffeeApiController {
     @PostMapping("/coffees")
     public @ResponseBody ResponseEntity<Coffee> create(@RequestBody CoffeeForm dto){
         // 1. Dto 2 Entity
-        Coffee newCoffee = dto.toEntity();
+        Coffee newCoffee = coffeeService.create(dto);
         // 2. Save the data
-        return ResponseEntity.status(HttpStatus.OK).body(coffeeRepository.save(newCoffee));
+        return (newCoffee != null) ? ResponseEntity.status(HttpStatus.OK).body(newCoffee) : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
     //PATCH
     @PatchMapping("/coffees/{id}")
     public @ResponseBody ResponseEntity<Coffee> update(@PathVariable Long id, @RequestBody CoffeeForm dto){
-        // 1. Get New Data from dto
-        Coffee newCoffee = dto.toEntity();
-        // 2. Find original Entity
-        Coffee originCoffee = coffeeRepository.findById(id).orElse(null);
-        if (originCoffee == null){
-            log.info("Bad Request ! ID : {}, Coffee : {}", id, newCoffee.toString());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        // 3. move to data.
-        originCoffee.patch(newCoffee);
-        // 4. update the data
-        Coffee updated = coffeeRepository.save(originCoffee);
-        return ResponseEntity.status(HttpStatus.OK).body(updated);
+        Coffee updated = coffeeService.update(id, dto);
+        return (updated != null) ? ResponseEntity.status(HttpStatus.OK).body(updated) : ResponseEntity.status(HttpStatus.BAD_REQUEST).body(dto.toEntity());
     }
     //DELETE
     @DeleteMapping("/coffees/{id}")
     public @ResponseBody ResponseEntity<Coffee> delete(@PathVariable Long id){
-        // 1. Get delete data.
-        Coffee target = coffeeRepository.findById(id).orElse(null);
-        if (target == null){
-            log.info("BAD REQUEST ID : {}", id);
-           return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        //2. delete the data
-        coffeeRepository.delete(target);
-        //3. return success
-        return ResponseEntity.status(HttpStatus.OK).body(null);
+        Coffee deleted = coffeeService.delete(id);
+        //NO_CONTENT -> 요청 내용이 잘 수행 되었고, 딱히 return 할 내용이 없을 때
+        //OK -> 요청 내용이 잘 수행 되었고, return 할 내용이 있을 때
+        return (deleted != null) ? ResponseEntity.status(HttpStatus.NO_CONTENT).build() : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 }
