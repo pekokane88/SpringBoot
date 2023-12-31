701cd53b9cce2c31a490fbd11c7cb473f3469a41
pekokane88 <pekokane88@gmail.com>
2023-12-31 23:05:52 +0900

    1. add mysql connector dependencies 2. add mysql properties

--- FirstProject/src/main/java/com/example/FirstProject/repository/PizzaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.FirstProject.repository;
-
-import com.example.FirstProject.entity.Pizza;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PizzaRepository extends JpaRepository<Pizza, Long> {
-
-}
